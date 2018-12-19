d8123cadd30bb2fc7511d1a077352276de4c6c69
Leonid Krutovsky <leonid.krutovsky@invitae.com>
2018-12-19 18:10:06 +0700

    master: Link filesytem lib, add file reading

--- FileSorter/main.cpp
+++ FileSorter/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <filesystem>
+#include <string>
+#include <fstream>
 
-using namespace std;
+namespace fs = std::filesystem;
 
-int main()
+std::string read_file(const fs::path & path)
 {
-    cout << "Hello World!" << endl;
+    std::ifstream file(path.u8string());
+    std::string buffer(fs::file_size(path), ' ');
+    buffer.assign((std::istreambuf_iterator<char>(file)),
+                std::istreambuf_iterator<char>());
+    return buffer;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " input_file" << " output_file" << std::endl;
+        return 1;
+    }
+    const fs::path input_file_name = argv[1];
+    if (!fs::exists(input_file_name))
+    {
+        std::cerr << "No input file " << input_file_name << std::endl;
+    }
+    const fs::path output_file_name = argv[2];
+
+    const auto buffer = read_file(input_file_name);
+    std::cout << buffer << std::endl;
     return 0;
 }
